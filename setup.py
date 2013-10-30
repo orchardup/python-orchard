872325126d1d48da6a00f2c6df0e447f78642764
Ben Firshman <ben@firshman.co.uk>
2013-10-30 15:07:07 +0000

    Upgrade to requests 2.0.1

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     install_requires=[
         'docopt==0.6.1',
         'docker-py==0.2.0-orchard',
-        'requests==1.2.3',
+        'requests==2.0.1',
         'texttable==0.8.1',
     ],
     dependency_links=[
