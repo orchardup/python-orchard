9b45299bb34f1a2df93ef957d42bc00926a7e8df
Aanand Prasad <aanand.prasad@gmail.com>
2014-02-14 14:32:30 -0800

    Fix client instantation methods

--- orchard/__init__.py
+++ orchard/__init__.py
@@ -6,13 +6,13 @@ from .errors import BadRequest, AuthenticationFailed
 __version__ = '1.0.10'
 
 def with_token(token):
-    client = Client(base_url(), docker_host())
+    client = Client(base_url())
     client.token = token
     return client
 
 
 def with_username_and_password(username, password):
-    client = Client(base_url(), docker_host())
+    client = Client(base_url())
 
     try:
         client.token = client.request("POST", "/signin", data={"username": username, "password": password}, quiet=True)["token"]
@@ -23,7 +23,3 @@ def with_username_and_password(username, password):
 
 def base_url():
     return os.environ.get('ORCHARD_API_URL', 'https://orchardup.com/api/v1')
-
-
-def docker_host():
-    return os.environ.get('ORCHARD_DOCKER_HOST', 'https://%s.orchardup.net')
